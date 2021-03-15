8572b8ffcd72528b39fc955ba8233a32b90cffb8
Gevorg74 <80593715+Gevorg74@users.noreply.github.com>
2021-03-15 23:19:13 +0400

    Update homework1.js

--- homework1.js
+++ homework1.js
@@ -1,1 +1,3 @@
-
+// 1.  Գրել ծրագիր որը աշխատացնելիս console-ում կարտածի ամբողջ օպերատիվ հիշողությունը (total amount of system memory):
+let os = require('os');
+console.log("Architecturetotal amount of system memory: " + Math.ceil(os.totalmem()/(1024 ** 3)));
